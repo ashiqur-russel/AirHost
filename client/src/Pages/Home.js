728989aa47ce71737fa282ab4339f1a0b1e606ee
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-18 23:10:39 +0100

    service details route with hard coded data

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Spinner from "../Components/Spinner/Spinner";
 import SearchForm from "../Components/Form/SearchForm";
 import ExpCard from "../Components/Card/ExpCard";
+import HomeCard from "../Components/Card/HomeCard";
 import { Link } from "react-router-dom";
 const Home = () => {
   const { loading, setLoading } = useState(false);
@@ -20,7 +21,22 @@ const Home = () => {
         <SearchForm />
       </div>
       <div className="flex-1">
-        <div>Home Card</div>
+        <div className="">
+          <div className="flex justify-between px-4 mt-10">
+            <p className="text-xl font-bold">Homes</p>
+            <Link to="/coming-soon">
+              <p>See All</p>
+            </Link>
+          </div>
+          <div className="container pb-8 pt-2 mx-auto">
+            <div className="flex flex-wrap">
+              {[...Array(3)].map((_, i) => (
+                <HomeCard key={i} />
+              ))}
+            </div>
+          </div>
+        </div>
+
         <div className="">
           <div className="flex justify-between px-4">
             <p className="text-xl font-bold">Experiences</p>
--- client/src/Routes/routes.js
+++ client/src/Routes/routes.js
@@ -1,31 +1,40 @@
-import React from 'react'
-import { createBrowserRouter } from 'react-router-dom'
-import ErrorPage from '../Pages/Shared/ErrorPage'
-import Home from '../Pages/Home'
-import Login from '../Pages/Login/Login'
-import Signup from '../Pages/Login/Signup'
-import Main from '../Layout/Main'
-
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../Pages/Shared/ErrorPage";
+import Home from "../Pages/Home";
+import Login from "../Pages/Login/Login";
+import Signup from "../Pages/Login/Signup";
+import Main from "../Layout/Main";
+import CommingSoon from "../Pages/Shared/ComingSoon.js";
+import Details from "../Pages/Details";
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Main />,
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path: '/login',
+        path: "/login",
         element: <Login />,
       },
       {
-        path: '/signup',
+        path: "/signup",
         element: <Signup />,
+      },
+      {
+        path: "/coming-soon",
+        element: <CommingSoon />,
+      },
+      {
+        path: "/service-details",
+        element: <Details />,
       },
     ],
   },
-])
+]);
 
-export default router
+export default router;
