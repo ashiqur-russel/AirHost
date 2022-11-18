cd19f5baf083d5e758178c3ff398e7846a906d0e
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-18 22:32:53 +0100

    experience fake data load on ui

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,11 +1,44 @@
-import React from 'react'
-import Spinner from '../Components/Spinner/Spinner'
+import React, { useState, useEffect } from "react";
+import Spinner from "../Components/Spinner/Spinner";
+import SearchForm from "../Components/Form/SearchForm";
+import ExpCard from "../Components/Card/ExpCard";
+import { Link } from "react-router-dom";
 const Home = () => {
+  const { loading, setLoading } = useState(false);
+  const [allExp, setAllExp] = useState([]);
+  useEffect(() => {
+    fetch("expdata.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setAllExp(data);
+      });
+  }, []);
   return (
-    <>
-      <Spinner />
-    </>
-  )
-}
+    <div className="md:flex justify-center gap-10 px-6 md:px-10 lg:px-20">
+      <div className="mt-4">
+        <SearchForm />
+      </div>
+      <div className="flex-1">
+        <div>Home Card</div>
+        <div className="">
+          <div className="flex justify-between px-4">
+            <p className="text-xl font-bold">Experiences</p>
+            <Link to="/coming-soon">
+              <p>See All</p>
+            </Link>
+          </div>
+          <div className="container pb-8 pt-2 mx-auto">
+            <div className="flex flex-wrap">
+              {allExp.slice(0, 4).map((exp, i) => (
+                <ExpCard key={i} exp={exp} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
