45ac7b050a9de49e8acb6f8c3b6d4442f29bb2b1
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-18 20:01:22 +0100

    jwt token generate upon user creation

--- server/index.js
+++ server/index.js
@@ -1,38 +1,55 @@
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
-const app = express()
-const port = process.env.PORT || 8000
+const app = express();
+const port = process.env.PORT || 8000;
 
 // middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Database Connection
-const uri = process.env.DB_URI
+const uri = process.env.DB_URI;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
-})
+});
 
 async function run() {
   try {
-    const homesCollection = client.db('aircncdb').collection('homes')
+    const homesCollection = client.db("aircnc-db").collection("homes");
+    const usersCollection = client.db("aircnc-db").collection("users");
 
-    console.log('Database Connected...')
+    //save user email & generate jwt
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = usersCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ token, result });
+    });
+    console.log("Database Connected...");
   } finally {
   }
 }
 
-run().catch(err => console.error(err))
+run().catch((err) => console.error(err));
 
-app.get('/', (req, res) => {
-  res.send('Server is running...')
-})
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
 
 app.listen(port, () => {
-  console.log(`Server is running...on ${port}`)
-})
+  console.log(`Server is running...on ${port}`);
+});
