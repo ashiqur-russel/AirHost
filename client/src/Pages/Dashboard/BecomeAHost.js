8110f7ca194634d2f8830e5cc292d67e85dd7e56
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-22 23:21:38 +0100

    Function created for image upload

--- client/src/Pages/Dashboard/BecomeAHost.js
+++ client/src/Pages/Dashboard/BecomeAHost.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { getImageUrl } from "../../api/imageUpload";
 import BecomeHostForm from "../../Components/Form/BecomeHostForm";
 
 const BecomeAHost = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const location = event.target.location.value;
+    const image = event.target.image.files[0];
+    getImageUrl(image).then((data) => {
+      console.log(data);
+    });
+  };
+
   return (
     <>
-      <BecomeHostForm></BecomeHostForm>
+      <BecomeHostForm handleSubmit={handleSubmit}></BecomeHostForm>
     </>
   );
 };
